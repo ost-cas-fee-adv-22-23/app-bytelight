a39223097c08abc3d5df83850f5aa4cdabc6556b
Marco Baumgartner <marco.baumgartner94@gmail.com>
2023-02-02 13:31:20 +0100

    feat: add prettier, eslint configuration and ci config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
 import { Heading1 } from '@smartive-education/design-system-component-library-bytelight';
 import { GetServerSideProps, InferGetStaticPropsType } from 'next';
 
-type PageProps = {};
-
-export default function PageHome({}: PageProps): InferGetStaticPropsType<typeof getServerSideProps> {
+export default function PageHome(): InferGetStaticPropsType<typeof getServerSideProps> {
   return (
     <>
       <Heading1>Hallo Matthias</Heading1>
     </>
   );
 }
+
 export const getServerSideProps: GetServerSideProps = async () => ({
   props: { posts: require('../data/posts.json') },
 });

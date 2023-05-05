8d59a410c9e5027a52bfd97f357056e6696be86e
matthias <matthias@smartive.ch>
2023-05-05 14:07:45 +0200

    change

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,6 +10,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  webServer: { command: 'npm run dev', url: 'http://localhost:3000' },
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,

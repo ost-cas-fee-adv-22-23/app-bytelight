68c96ccabdb4fa1ad7363483b431740f7c021590
matthias <matthias@smartive.ch>
2023-06-18 14:32:14 +0200

    delet normal user

--- tests/auth-setup.spec.ts
+++ tests/auth-setup.spec.ts
@@ -4,9 +4,9 @@ test('test login screen', async ({ page }) => {
   await page.goto('http://localhost:3000/api/auth/signin?csrf=true');
   await page.getByRole('button', { name: 'Sign in with zitadel' }).click();
   const input = page.getByPlaceholder('username@domain');
-  await input.fill('mb@smartive.zitadel.cloud');
+  await input.fill('test-bytelight@smartive.zitadel.cloud');
   await page.getByText('next').click();
-  await page.getByLabel('Password').fill('Bfcuk1994pn1994!');
+  await page.getByLabel('Password').fill('Bytelight1994!');
   await page.getByText('next').click();
   await page.getByRole('heading', { name: 'Willkommen auf Mumble' }).click();
 });

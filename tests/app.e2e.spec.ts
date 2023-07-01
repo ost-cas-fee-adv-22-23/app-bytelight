a0dd02cec7f8de85a77148171f19e9ceca99425f
Marco Baumgartner <marco.baumgartner94@gmail.com>
2023-07-01 10:57:46 +0200

    change regex

--- tests/app.e2e.spec.ts
+++ tests/app.e2e.spec.ts
@@ -9,45 +9,45 @@ test.describe('Mumble e2e Tests', () => {
   test('create a post', async ({ page }) => {
     expect(page.getByRole('heading', { name: 'Hey was gibts neues?' })).toBeVisible();
     await page.getByPlaceholder('Deine Meinung zählt').fill(`Ich bin ein TEST! id: ${testIdPost}`);
-    await Promise.all([page.waitForResponse(/\/posts/), page.getByRole('button', { name: 'Absenden' }).click()]);
+    await Promise.all([page.waitForResponse(/\/posts/), await page.getByRole('button', { name: 'Absenden' }).click()]);
     expect(await page.getByText(`Ich bin ein TEST! id: ${testIdPost}`).isVisible());
   });
 
   test('delete a post', async ({ page }) => {
     await page.getByPlaceholder('Deine Meinung zählt').fill(`Ich bin ein TEST, delete! id: ${testIdPost}`);
-    await Promise.all([page.waitForResponse(/\/posts/), page.getByRole('button', { name: 'Absenden' }).click()]);
+    await Promise.all([page.waitForResponse(/\/posts/), await page.getByRole('button', { name: 'Absenden' }).click()]);
     expect(page.getByText(`Ich bin ein TEST, delete! id: ${testIdPost}`).isVisible());
     // expect(await page.getByTestId('go-to-post').first().waitFor());
     // await page.getByTestId('go-to-post').first().click();
-    // await page.waitForURL(/.*\/mumble\/.+/);
-    // await expect(page).toHaveURL(/.*\/mumble\/.+/);
+    // await page.waitForURL(/\/mumble\/[0-9A-Z]{20}/);
+    // await expect(page).toHaveURL(/\/mumble\/[0-9A-Z]{20}/);
     // expect(await page.getByText(`Ich bin ein TEST, delete! id: ${testIdPost}`).isVisible());
-    await Promise.all([page.waitForResponse(/\/posts/), page.getByRole('button', { name: 'Delete' }).first().click()]);
+    await Promise.all([page.waitForResponse(/\/posts/), await page.getByRole('button', { name: 'Delete' }).first().click()]);
   });
 
   test('like a post', async ({ page }) => {
     await page.getByPlaceholder('Deine Meinung zählt').fill(`Ich bin ein TEST! Für einen Like! id: ${testIdPost}`);
-    await Promise.all([page.waitForResponse(/\/posts/), page.getByRole('button', { name: 'Absenden' }).click()]);
+    await Promise.all([page.waitForResponse(/\/posts/), await page.getByRole('button', { name: 'Absenden' }).click()]);
     expect(page.getByText(`Ich bin ein TEST! Für einen Like! id: ${testIdPost}`).isVisible());
     await page.getByTestId('go-to-post').first().click();
-    await page.waitForURL(/.*\/mumble\/.+/);
-    await expect(page).toHaveURL(/.*\/mumble\/.+/);
+    await page.waitForURL(/\/mumble\/[0-9A-Z]{20}/);
+    await expect(page).toHaveURL(/\/mumble\/[0-9A-Z]{20}/);
     expect(page.getByText(`Ich bin ein TEST! Für einen Like! id: ${testIdPost}`));
-    await Promise.all([page.waitForResponse(/\/posts/), page.getByRole('button', { name: '0 Likes' }).click()]);
+    await Promise.all([page.waitForResponse(/\/posts/), await page.getByRole('button', { name: '0 Likes' }).click()]);
     expect(await page.getByRole('button', { name: '1 Like' }).isVisible());
   });
 
   test('comment a post', async ({ page }) => {
     await page.getByPlaceholder('Deine Meinung zählt').fill(`Ich bin ein TEST! Für einen Kommentar! id: ${testIdPost}`);
-    await Promise.all([page.waitForResponse(/\/posts/), page.getByRole('button', { name: 'Absenden' }).click()]);
+    await Promise.all([page.waitForResponse(/\/posts/), await page.getByRole('button', { name: 'Absenden' }).click()]);
     expect(await page.getByText(`Ich bin ein TEST! Für einen Kommentar! id: ${testIdPost}`).isVisible());
     expect(await page.getByTestId('go-to-post').first().waitFor());
     await page.getByTestId('go-to-post').first().click();
-    await page.waitForURL(/.*\/mumble\/.+/);
-    await expect(page).toHaveURL(/.*\/mumble\/.+/);
+    await page.waitForURL(/\/mumble\/[0-9A-Z]{20}/);
+    await expect(page).toHaveURL(/\/mumble\/[0-9A-Z]{20}/);
     expect(await page.getByText(`Ich bin ein TEST! Für einen Kommentar! id: ${testIdPost}`).isVisible());
     await page.getByPlaceholder('Deine Meinung zählt').fill('Ich bin ein Kommentar!');
-    await Promise.all([page.waitForResponse(/\/posts/), page.getByRole('button', { name: 'Absenden' }).click()]);
+    await Promise.all([page.waitForResponse(/\/posts/), await page.getByRole('button', { name: 'Absenden' }).click()]);
     expect(await page.getByText('Ich bin ein Kommentar!').isVisible());
   });
 
@@ -57,8 +57,8 @@ test.describe('Mumble e2e Tests', () => {
     expect(await page.getByText(`Ich bin ein TEST! Für einen Comment! id: ${testIdPost}`).isVisible());
     expect(await page.getByTestId('go-to-post').first().waitFor());
     await page.getByTestId('go-to-post').first().click();
-    await page.waitForURL(/.*\/mumble\/.+/);
-    await expect(page).toHaveURL(/.*\/mumble\/.+/);
+    await page.waitForURL(/\/mumble\/[0-9A-Z]{20}/);
+    await expect(page).toHaveURL(/\/mumble\/[0-9A-Z]{20}/);
     expect(await page.getByText(`Ich bin ein TEST! Für einen Comment! id: ${testIdPost}`).isVisible());
   });
 });

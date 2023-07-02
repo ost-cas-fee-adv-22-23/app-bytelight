c8ba635ae318420a3f6d8b8b327c37ab23028c28
Marco Baumgartner <marco.baumgartner94@gmail.com>
2023-07-02 09:19:40 +0200

    update release action

--- tests/app.e2e-snapshot.spec.ts
+++ tests/app.e2e-snapshot.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test';
 
 test.describe('Visual comparison', () => {
   test('test logout screen', async ({ page }) => {
-    await page.goto('http://localhost:3000/');
+    await page.goto('/');
     await page.getByRole('button', { name: 'Log Out' }).click();
     await page.waitForLoadState('networkidle');
     expect(page.getByRole('heading', { name: 'Hey there.' })).toBeTruthy();
